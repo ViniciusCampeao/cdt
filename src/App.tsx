9c775e90d9683da0821b7fd5aa5a86b5a7e7da14
ViniciusCampeao <viniciusfernandes643@gmail.com>
2024-12-20 15:05:59 -0300

    feat: new modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Home from "./pages/home";
 import { init } from "@emailjs/browser";
+import Home from "./pages/Init";
 import PaymentPage from "./pages/PaymentPage";
 import Thanks from "./pages/Thanks";
 
