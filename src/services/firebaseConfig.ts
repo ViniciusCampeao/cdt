0251463c537961d0cd3a03b4ee2c916b61ede140
ViniciusCampeao <viniciusfernandes643@gmail.com>
2024-12-23 11:35:59 -0300

    feat: add more security

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsuIKmk4Xx_CjgtzhHxemiqcfAUyhnoqo",
-  authDomain: "cdtpg-38cd9.firebaseapp.com",
-  projectId: "cdtpg-38cd9",
-  storageBucket: "cdtpg-38cd9.firebasestorage.app",
-  messagingSenderId: "952828864334",
-  appId: "1:952828864334:web:f202b4bada5c3f2e82e01f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
