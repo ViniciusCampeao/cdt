ebf7c2c5bd6a98151c802bf60fdf349009870b5f
ViniciusCampeao <viniciusfernandes643@gmail.com>
2025-01-07 09:13:54 -0300

    feat: extraction and correction numbers component

--- src/components/Extract/index.tsx
+++ src/components/Extract/index.tsx
@@ -1,77 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const PhoneExtractor: React.FC = () => {
-  const [text, setText] = useState('');
-  const [phones, setPhones] = useState<{ number: string; checked: boolean }[]>([]);
+  const [numeros, setNumeros] = useState<string>("");
+  const [numerosCorrigidos, setNumerosCorrigidos] = useState<string>("");
 
-  const extractPhoneNumbers = (inputText: string) => {
-    // Atualize a expressão regular para corresponder a números com 8 a 13 dígitos
-    const phonePattern = /\b(\d{8,13})\b/g;
-    const matches = inputText.match(phonePattern);
-    if (matches) {
-      const formattedNumbers = matches.map(number => {
-        // Formate os números conforme necessário
-        if (number.length === 10) {
-          return { number: number.slice(0, 2) + '9' + number.slice(2), checked: false };
-        }
-        return { number, checked: false };
-      });
-      setPhones(formattedNumbers);
-    } else {
-      setPhones([]);
-    }
+  const corrigirNumeros = (numeros: string[]): string => {
+    const numerosCorrigidos: string[] = numeros.map((numero) => {
+      numero = numero.trim();
+
+      if (!numero.startsWith("+55")) {
+        numero = "+55" + numero;
+      }
+
+      if (numero[3] !== '4' || numero[4] !== '3') {
+        numero = numero.slice(0, 3) + '43' + numero.slice(3);
+      }
+
+      while (numero.length < 14) {
+        numero = numero.slice(0, 5) + '9' + numero.slice(5);
+      }
+
+      return numero;
+    });
+
+    return numerosCorrigidos.join(", ");
   };
 
-  const handleExtract = () => {
-    extractPhoneNumbers(text);
-  };
-
-  const handleCheckboxChange = (index: number) => {
-    setPhones(prevPhones =>
-      prevPhones.map((phone, i) =>
-        i === index ? { ...phone, checked: !phone.checked } : phone
-      )
-    );
+  const processar = () => {
+    const listaNumeros = numeros.split(/\s+/).map((num) => num.trim());
+    const numerosCorrigidos = corrigirNumeros(listaNumeros);
+    setNumerosCorrigidos(numerosCorrigidos);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-3xl font-bold mb-4">Extrator de Números de Telefone</h1>
+    <div className="flex flex-col items-center bg-green-100 p-8 rounded-lg max-w-xl mx-auto my-12">
+      <h1 className="font-sans text-2xl mb-5">Extração e correção de Números de Telefone</h1>
+      <label htmlFor="numeros" className="mb-2 font-sans text-lg">
+        Insira os números:
+      </label>
       <textarea
-        className="w-full md:w-1/2 p-2 mb-4 border border-gray-300 rounded"
-        placeholder="Insira o texto aqui..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+        id="numeros"
+        rows={10}
+        className="w-full p-2 font-sans text-base border-2 border-green-500 rounded-lg mb-5"
+        value={numeros}
+        onChange={(e) => setNumeros(e.target.value)}
       />
       <button
-        className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-        onClick={handleExtract}
+        onClick={processar}
+        className="px-4 py-2 text-lg bg-green-500 text-white border-none rounded-lg cursor-pointer mb-5"
       >
-        Extrair Números
+        Corrigir Números
       </button>
-      <div className="mt-4 w-full md:w-1/2">
-        <h2 className="text-2xl font-semibold mb-2">Números Extraídos:</h2>
-        <ul className="list-disc list-inside">
-          {phones.map((phone, index) => (
-            <li key={index} className="bg-white p-2 my-1 rounded border border-gray-300 flex items-center">
-              <input
-                type="checkbox"
-                checked={phone.checked}
-                onChange={() => handleCheckboxChange(index)}
-                className="mr-2"
-              />
-              <a
-                href={`https://wa.me/${phone.number}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={`flex-1 ${phone.checked ? 'line-through text-gray-500' : ''}`}
-              >
-                {phone.number}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <label htmlFor="numerosCorrigidos" className="mb-2 font-sans text-lg">
+        Números Corrigidos:
+      </label>
+      <textarea
+        id="numerosCorrigidos"
+        rows={5}
+        readOnly
+        className="w-full p-2 font-sans text-base border-2 border-green-500 rounded-lg mb-5"
+        value={numerosCorrigidos}
+      />
     </div>
   );
 };
